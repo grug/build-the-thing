6c3d603f76e60973a5194811873eeb28c6aa3f05
Dave Cooper <dave@davecooper.dev>
2020-07-29 20:12:35 +0100

    Restart

--- pages/api/random-thing.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json({
-    randomThing: Math.floor(Math.random() * 10) + 1,
-  });
-};
--- pages/things.tsx
+++
@@ -1,3 +0,0 @@
-export default function Things() {
-  return <div>More things!</div>;
-}
