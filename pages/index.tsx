2673a4d78e3cc795d3d51cb544fc05cac9f14036
Dave Cooper <dave@davecooper.dev>
2020-08-06 17:52:58 +0100

    Built the thing

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import styles from "../styles/Home.module.css";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>My things!</h1>
+      <RandomThing />
+    </div>
+  );
+}
+
+const RandomThing = () => {
+  const { data } = useSWR<{ randomThing: number }>(
+    "/api/random-thing",
+    fetcher,
+    {
+      revalidateOnFocus: false,
+    }
+  );
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>Random thing: {data.randomThing}</div>;
+};
